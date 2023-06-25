002c1bfa384dd410c4addaa6e08f2865a5725225
rahul.b <rahul.b@simformsolutions.com>
2023-06-25 17:26:44 +0530

    Add automated readme.md update code

---
+++ index.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+const Mustache = require('mustache');
+const axios = require('axios');
+
+const fs = require('fs');
+
+const MUSTACHE_MAIN_DIR = './main.mustache';
+
+let DATA = {
+  refresh_date: new Date().toLocaleDateString('en-GB', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
+    timeZone: 'Asia/Calcutta',
+  }),
+};
+
+async function setWeatherInformation() {
+  await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=ahmedabad&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
+  )
+    .then(response => {
+      let r =response.data;
+      DATA.city_temperature = Math.round(r.main.temp);
+      DATA.city_weather = r.weather[0].description;
+      DATA.city_weather_icon = r.weather[0].icon;
+      DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'Europe/Stockholm',
+      });
+      DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'Europe/Stockholm',
+      });
+    });
+}
+
+async function generateReadMe() {
+  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+    if (err) throw err;
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync('README.md', output);
+  });
+}
+
+async function action() {
+  /**
+   * Fetch Weather
+   */
+  await setWeatherInformation();
+
+  /**
+   * Generate README
+   */
+  await generateReadMe();
+}
+
+action();
